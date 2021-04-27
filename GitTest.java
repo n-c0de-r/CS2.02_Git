a5cfc8c6ec0cb7338a69c14c3549270a05604f8d
Elisa <elisa.doernbrak01@gmail.com>
2021-04-27 17:54:04 +0200

    Viel Spaß bei der Aufgabe :)

---
+++ GitTest.java
@@ -0,0 +1,22 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		
+		
+		loneSum(1, 2, 3);
+		loneSum(3, 2, 3);
+		loneSum(3, 3, 3); 
+		
+
+	}
+	
+	public static int loneSum(int a, int b, int c) {
+		
+//		//Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values,
+//		  it does not count towards the sum
+		
+		throw new java.lang.UnsupportedOperationException("Not implemented yet.");
+	}
+
+}
